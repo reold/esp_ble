d69fb370ed6d3840294279ab7655fffe7edfb855
benjamin-feldman <benjamin.feldman666@gmail.com>
2025-08-10 18:57:02 +0200

    Initial Commit

---
+++ esp_ble/esp_bleApp.swift
@@ -0,0 +1,32 @@
+//
+//  esp_bleApp.swift
+//  esp_ble
+//
+//  Created by Benjamin Feldman on 10/08/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct esp_bleApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
